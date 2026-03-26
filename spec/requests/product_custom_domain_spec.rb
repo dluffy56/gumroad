9dff74ba6678ce18b90cc6c42717b0f1e7d96052
dluffy56 <bbheemaraya944@gmail.com>
2026-03-26 13:07:27 +0530

    fix

--- spec/requests/product_custom_domain_spec.rb
+++ spec/requests/product_custom_domain_spec.rb
@@ -21,6 +21,15 @@ describe "ProductCustomDomainScenario", type: :system, js: true do
     expect(product.sales.successful.count).to eq(1)
   end
 
+  context "when the URL includes the product permalink" do
+    it "successfully purchases the linked product" do
+      visit "http://#{custom_domain.domain}:#{port}/l/#{product.unique_permalink}"
+      click_on "I want this!"
+      check_out(product)
+      expect(product.sales.successful.count).to eq(1)
+    end
+  end
+
   context "when buyer is logged in" do
     let(:buyer) { create(:user) }
     before do
@@ -32,6 +41,21 @@ describe "ProductCustomDomainScenario", type: :system, js: true do
       click_on "I want this!"
       expect(page).to have_field("Email address", with: buyer.email, disabled: true)
       check_out(product, logged_in_user: buyer)
+      expect(product.sales.successful.count).to eq(1)
+    end
+  end
+
+  context "when there is a discount code" do
+    let(:product) { create(:product, price_cents: 600) }
+    let!(:offer_code) { create(:percentage_offer_code, products: [product], code: "LAUNCH", amount_percentage: 50) }
+
+    it "applies the discount code when the URL contains both the product permalink and the discount code" do
+      visit "http://#{custom_domain.domain}:#{port}/l/#{product.unique_permalink}/#{offer_code.code}"
+
+      expect(page).to have_text("50% off will be applied at checkout (Code LAUNCH)")
+
+      click_on "I want this!"
+      check_out(product)
       expect(product.sales.successful.count).to eq(1)
     end
   end
